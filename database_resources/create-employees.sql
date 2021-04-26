08c9c28ca5a2dd830a868b1429691e22ba577e5c
Paul <pakaras2@illinois.edu>
2021-04-26 03:01:58 -0500

    skeleton commit

---
+++ database_resources/create-employees.sql
@@ -0,0 +1,11 @@
+-- public.employees definition
+
+-- Drop table
+
+-- DROP TABLE public.employees;
+
+CREATE TABLE public.employees (
+	id int8 NOT NULL,
+	username varchar NOT NULL,
+	"password" varchar NOT NULL
+);
---
+++ src/bank/User.java
@@ -0,0 +1,6 @@
+package bank;
+
+public interface User {
+	int logOn(User user);
+	int logOff(User user);
+}
